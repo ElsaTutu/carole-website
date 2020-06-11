aef4b1cf18e184db05096b892be15ace9b789252
ElsaTutu <elsa.tuilliere@gmail.com>
2020-06-11 16:10:01 +0100

    adding slideshow and card of articles

--- content/themes/carole/app/js/app.js
+++ content/themes/carole/app/js/app.js
@@ -1,6 +1,27 @@
 var app = {
   init: function() {
     console.log('init');
+    
+    // slideshow
+    var slideIndex = 0;
+    showSlides();
+
+    function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 5000); // Change image every 5 seconds
+    }
   },
 };
 
@@ -62,25 +83,32 @@ var TxtType = function(el, toRotate, period) {
         document.body.appendChild(css);
     };
 
-    /*Scroll to top when arrow up clicked BEGIN*/
-$(window).scroll(function() {
-    var height = $(window).scrollTop();
-    if (height > 100) {
-        $('#back2Top').fadeIn();
-    } else {
-        $('#back2Top').fadeOut();
-    }
-});
-$(document).ready(function() {
-    $("#back2Top").click(function(event) {
-        event.preventDefault();
-        $("html, body").animate({ scrollTop: 0 }, "slow");
-        return false;
-    });
 
-});
- /*Scroll to top when arrow up clicked END*/
+    /*Scroll to top when arrow up clicked BEGIN*/
+        $(window).scroll(function() {
+            var height = $(window).scrollTop();
+            if (height > 100) {
+                $('#back2Top').fadeIn();
+            } else {
+                $('#back2Top').fadeOut();
+            }
+        });
+        $(document).ready(function() {
+            $("#back2Top").click(function(event) {
+                event.preventDefault();
+                $("html, body").animate({ scrollTop: 0 }, "slow");
+                return false;
+            });
+
+        });
+        
+        // slideshow
+        
+                
+
+      
 
 
 $(app.init);
+$(app.slideshow);
 // $(app.menubar);
